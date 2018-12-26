1ac8a0be6b2c3d60d3d51e4c69e2ffb1b31efbfc
robertgrochowski <robert.grochowski98@gmail.com>
2018-12-26 23:21:51 +0100

    update

--- src/skj/pro2/client/Client.java
+++ src/skj/pro2/client/Client.java
@@ -1,27 +1,66 @@
 package skj.pro2.client;
 
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
+import java.io.*;
+import java.net.*;
 
 public class Client
 {
-    //TODO: try exceptions, wrong server adress
-    public static void main(String[] args) throws IOException {
-        DatagramSocket ds = new DatagramSocket();
+    private ServerSocket serverSocket;
+    private boolean hasConnection;
 
-        // DatagramPacket dpToSend = new DatagramPacket(packetByte, packetByte.length, InetAddress.getByName(serverAddress), serverPort);
+    public class TCPListener implements Runnable {
+        @Override
+        public void run() {
+            try {
+                Socket socket = serverSocket.accept();
+                hasConnection = true;
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+                String request = br.readLine();
+                System.out.println("Read TCP: " + request);
+
+                serverSocket.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private Client(){
+        hasConnection = false;
+    }
+
+    private void run(String[] args) throws IOException, InterruptedException {
+
+        //Opening serverSocket, assuming that last given port is our TCP listener port as well
+        int TCPPort = Integer.parseInt(args[args.length-1]);
+        serverSocket = new ServerSocket(TCPPort);
+        TCPListener listener = new TCPListener();
+        new Thread(listener).start();
+
+
+        DatagramSocket ds = new DatagramSocket();
 
         String serverAddress = args[0];
         for(int i = 1; i < args.length; i++)
         {
-            byte[] packet = ("Package #" + i).getBytes();
+            byte[] packet = ("SEQ_REQ:" + (i-1)).getBytes();
             DatagramPacket dpToSend = new DatagramPacket(packet, packet.length, InetAddress.getByName(serverAddress), Integer.parseInt(args[i]));
             ds.send(dpToSend);
             System.out.println("package sent: "+i);
         }
 
+        //Wait for conection
+        Thread.sleep(5000);
+        if(!hasConnection)
+            serverSocket.close();
+    }
+
+    //TODO: try exceptions, wrong server adress, no ports, bad syntax, no last port?
+    public static void main(String[] args) throws IOException, InterruptedException {
+        new Client().run(args);
     }
 }
--- src/skj/pro2/server/Server.java
+++ src/skj/pro2/server/Server.java
@@ -2,24 +2,118 @@ package skj.pro2.server;//otwieramy zadana parametrem ilosc portow UDP i numery 
 
 import java.io.IOException;
 import java.net.*;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Server {
 
-    public static void main(String[] args) throws IOException {
+    private static final long SEQUENCE_TIMEOUT = 5000;
 
-        InetAddress adressSerweraDNS = InetAddress.getByName("localhost");
-        DatagramSocket ds = new DatagramSocket(5011, adressSerweraDNS);
-//test
-        while(true)
-        {
-            DatagramPacket dpToRecv = new DatagramPacket(new byte[1460], 1460);
-            ds.receive(dpToRecv);
-            System.out.println("received");
-            String string = new String(dpToRecv.getData());
-            System.out.println(string);
+    private InetAddress serverAddress;
+    private int[] validPortSeq;
+    private Map<InetAddress, Boolean[]> candidates;
+    private Map<InetAddress, Long> lastCheck;
+
+    class UDPListener implements Runnable{
+        private int port;
+
+        @Override
+        public void run() {
+            try {
+                DatagramSocket socket = new DatagramSocket(port, serverAddress);
+                DatagramPacket packet = new DatagramPacket(new byte[1460], 1460);
+
+                socket.receive(packet);
+                String received = new String(packet.getData());
+                System.out.println("RECV: "+received);
+
+                String[] split = received.trim().split(":");
+                int seqNo = Integer.parseInt(split[1]);
+
+                synchronized(this)
+                {
+                    Boolean[] array;
+                    System.out.println("Connecting IP: " + packet.getAddress());
+                    if(candidates.containsKey(packet.getAddress()))
+                    {
+                        array = candidates.get(packet.getAddress());
+                    } else array = new Boolean[validPortSeq.length];
+
+                    for(int i = 0; i < validPortSeq.length; i++)
+                    {
+                        if(validPortSeq[i] == port) {
+                            array[i] = seqNo == i;
+                            System.out.println("Port " + port + " approved? :" + array[i]);
+                        }
+                    }
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        UDPListener(int port){
+            this.port = port;
         }
     }
 
+    private Server() throws UnknownHostException {
+        serverAddress = InetAddress.getByName("localhost");
+        candidates = new HashMap<>();
+        lastCheck = new HashMap<>();
+    }
 
+    private void run(String[] args) {
+        validPortSeq = new int[args.length];
+
+        for(int i = 0; i < args.length; i++) {
+            validPortSeq[i] = Integer.parseInt(args[i]);
+            new Thread(new UDPListener(validPortSeq[i])).start();
+        }
+
+        System.out.println("Server started");
+        //Thread for validating
+        while(true)
+        {
+            synchronized(this)
+            {
+                for(Map.Entry<InetAddress, Boolean[]> entry : candidates.entrySet())
+                {
+                    Boolean[] accessGranted = entry.getValue();
+                    boolean everyoneGrantedAccess = true;
+                    boolean everyoneResponded = true;
+
+                    for (int i = 0; i < entry.getValue().length; i++)
+                    {
+                        if(accessGranted[i] == null)
+                            everyoneResponded = false;
+                        else if(!accessGranted[i])
+                        {
+                            everyoneGrantedAccess = false;
+                            break;
+                        }
+                    }
+
+                    if (everyoneGrantedAccess) {
+                        //START TCP
+                        System.out.println("Everyone granted the access! starting tcp");
+                    }
+                    else
+                    {
+                        if (everyoneResponded || System.currentTimeMillis() - lastCheck.get(entry.getKey()) > SEQUENCE_TIMEOUT) {
+                            candidates.remove(entry.getKey());
+                            lastCheck.remove(entry.getKey());
+                            System.out.println(entry.getKey() + "timeout...");
+                        }
+                        else lastCheck.replace(entry.getKey(), System.currentTimeMillis());
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Server().run(args);
+    }
 }
