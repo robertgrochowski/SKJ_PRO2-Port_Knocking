8eadbe21ddb69e4c575a631d92b442f5f93c3da6
Robert Grochowski <robert@dbit.com.pl>
2021-01-05 16:32:28 +0100

    fixed bug (double port in params)

--- run_multipleClients.bat
+++ run_multipleClients.bat
@@ -1,4 +1,4 @@
-start java -Dfile.encoding=UTF-8 -classpath "out" skj.pro2.server.Server 3002 3004 3003 3000 3001 3001
+start java -Dfile.encoding=UTF-8 -classpath "out" skj.pro2.server.Server 3002 3004 3003 3000 3001
 timeout 3
 cd multipleclients
 start run_client1.bat
--- run_timeout.bat
+++ run_timeout.bat
@@ -1,4 +1,4 @@
-start java -Dfile.encoding=UTF-8 -classpath "source/out" skj.pro2.server.Server 3002 3004 3003 3000 3001 3001
+start java -Dfile.encoding=UTF-8 -classpath "source/out" skj.pro2.server.Server 3002 3004 3003 3000 3001
 timeout 3
 java -Dfile.encoding=UTF-8 -classpath "source/out" skj.pro2.client.Client 127.0.0.1 3002 3004 3003
 pause
